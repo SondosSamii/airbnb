eafb6b7792842212a7bdfe2008c2d25fb31a85e0
esraa <esraakhattab529@gmail.com>
2021-02-02 08:50:50 -0800

    edit2

--- src/components/routes.js
+++ src/components/routes.js
@@ -2,12 +2,16 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Home from './home/home';
 import NotFound from './not-found';
+import ProfileEdit from './profile/profileEdit';
+import Profile from './profile/profileView';
 
 const Routes = ()=> {
     return(
         <Router>
             <Switch>
                 <Route exact path="/" component={Home}></Route>
+                <Route exact path="/profile" component={Profile}></Route>
+                <Route exact path="/profileEdit" component={ProfileEdit}></Route>
                 <Route path="*" component={NotFound}></Route>
             </Switch>
         </Router>
