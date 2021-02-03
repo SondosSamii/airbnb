0c8364c024e7b6b07181be47d3de00c1a3500ee0
Aya Ali <aya.ali466@gmail.com>
2021-02-03 20:00:12 +0200

    New Login & signup & Host Forms

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,13 @@
-const Home = ()=> {
+import { getSessionCookie, SessionContext } from "./session";
+import { useContext } from "react";
+
+
+const Home = ()=>
+ {const session = useContext(SessionContext);
     return (
         <div>
           <h1>Home Works</h1>
+          <p>{session.email}</p>
         </div>
     )
 }
--- src/components/routes.js
+++ src/components/routes.js
@@ -1,14 +1,34 @@
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { createBrowserHistory } from "history";
+import React, { useEffect, useState } from "react";
+import { getSessionCookie, SessionContext } from "./session";
 
 import Home from './home';
-
+import Host from '../components/Forms/host'
+import Login from '../components/Forms/login'
 const Routes = ()=> {
+    const history = createBrowserHistory();
+    const [session, setSession] = useState(getSessionCookie());
+    useEffect(
+        () => {
+            const cookie = getSessionCookie();
+            console.log(cookie);
+            console.log(session);
+            if(cookie.email !== session.email) {
+            setSession(getSessionCookie());} 
+    },
+    [session]
+  );
     return(
-        <Router>
+        <SessionContext.Provider value={session}>
+        <Router history={history}>
             <Switch>
                 <Route exact path="/" component={Home}></Route>
+                <Route exact path="/host" component={Host}></Route>
+                <Route exact path="/login" component={Login}></Route>
             </Switch>
         </Router>
+        </SessionContext.Provider>
     )
 }
 
