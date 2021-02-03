a769f2b8f3a16f3cf9ec7cfe3c97d2c984b1395c
esraa <esraakhattab529@gmail.com>
2021-02-03 15:04:50 -0800

    about page

--- src/components/routes.js
+++ src/components/routes.js
@@ -2,12 +2,14 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Home from './home/home';
 import NotFound from './not-found';
+import About from './about/about'; 
 
 const Routes = ()=> {
     return(
         <Router>
             <Switch>
                 <Route exact path="/" component={Home}></Route>
+                <Route path="/about" component={About}></Route>
                 <Route path="*" component={NotFound}></Route>
             </Switch>
         </Router>
