fd97ed68f79fecf1f7d3bc4914ec2e2e9c21276f
sondossamii <sondos.sami.abuzeid@gmail.com>
2021-02-03 15:15:45 +0200

    Add <Navbar/> and <Footer/> in all views

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,21 +1,15 @@
-import Navbar from "../navbar";
 import Slideshow from "./slideshow";
 import Places from "./places";
 import Highlights from "./highlights";
-import Footer from "../footer";
 
 const Home = () => {
     document.title = "Home";
     return (
-        <>
-          <Navbar/>
-          <section id="home">
-              <Slideshow/>
-              <Places/>
-              <Highlights/>
-              <Footer/>
-          </section>
-        </>
+        <section id="home">
+            <Slideshow/>
+            <Places/>
+            <Highlights/>
+        </section>
     )
 }
 
--- src/components/routes.js
+++ src/components/routes.js
@@ -1,15 +1,19 @@
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
+import Navbar from './navbar';
 import Home from './home/home';
 import NotFound from './not-found';
+import Footer from './footer';
 
 const Routes = ()=> {
     return(
         <Router>
+            <Navbar/>
             <Switch>
                 <Route exact path="/" component={Home}></Route>
                 <Route path="*" component={NotFound}></Route>
             </Switch>
+            <Footer/>
         </Router>
     )
 }
