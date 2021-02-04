23d3a55a3a37333e82a9b844c864c81fc47e0aa0
sondossamii <sondos.sami.abuzeid@gmail.com>
2021-02-04 14:20:32 +0200

    Add 'OurTeam' Componnet

---
+++ src/components/our-team.js
@@ -0,0 +1,11 @@
+import {SiGithub, SiFacebook, SiTwitter, SiLinkedin, SiInstagram} from "react-icons/si";
+
+const OurTeam = () => {
+    return (
+        <section id="team">
+            <h1>Team Works</h1>
+        </section>
+    )
+}
+
+export default OurTeam;
